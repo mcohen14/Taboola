be380cb590f2a8968d2cd8fe7d1242d45105a169
Matthew Cohen <57816722+mcohen14@users.noreply.github.com>
2022-09-11 14:34:33 -0700

    Update Node.java

--- Node.java
+++ Node.java
@@ -7,3 +7,5 @@ public class Node {
         this.num = num;
     }
 }
+
+// In order to support any data type we can use a class instead of a primitive data type so we can use null to represent a null node.
